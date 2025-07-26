0a4051fd34a7335371f345ff94819dd0212b7ad2
lwmacct <lwmacct@icloud.com>
2025-07-26 16:13:07 +0800

    feat: 添加 demo1 功能函数和文档

---
+++ pyproject/demo1/main.py
@@ -0,0 +1,16 @@
+def main():
+    print("Hello from demo1!")
+
+
+def get_message():
+    """返回一个消息字符串"""
+    return "Hello from demo1 function!"
+
+
+def calculate_sum(a, b):
+    """计算两个数的和"""
+    return a + b
+
+
+if __name__ == "__main__":
+    main()
