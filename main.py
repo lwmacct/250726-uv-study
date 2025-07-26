bc9077e3493183dd465fb7fd72621780ef2f66a5
lwmacct <lwmacct@icloud.com>
2025-07-26 16:22:01 +0800

    feat: 使用 uv init 重新创建 demo1 和 demo2 项目

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from 250726-uv-study!")
+
+
+if __name__ == "__main__":
+    main()
--- pyproject/demo1/demo1/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Demo1 package
-"""
-
-from .main import get_message, calculate_sum
-
-__all__ = ['get_message', 'calculate_sum']
