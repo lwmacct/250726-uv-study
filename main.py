81cf81b0ececf42878839c8651ea38f7c098aab3
lwmacct <lwmacct@icloud.com>
2025-07-26 22:43:22 +0800

    更新 main.py 和 pyproject.toml 中的包名引用

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import pkg1
+import pkg10
 
 
 def main():
     print("Hello from 250726-uv-study-master!")
-    print("Now calling pkg1:")
-    pkg1.main()
+    print("Now calling pkg10:")
+    pkg10.main()
 
 
 if __name__ == "__main__":
